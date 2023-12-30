69573292a0bc4c68579337d9f2428ea0ebb390a1
ShahdRamzy <shahd.ramzy123@gmail.com>
2023-12-30 04:07:00 +0200

    Update Homepage.js

--- frontend/src/Views/Homepage.js
+++ frontend/src/Views/Homepage.js
@@ -51,20 +51,20 @@ const Homepage=() =>{
                     <div>
                         <img className="sale" src={sale} alt='Sale'/>
                     </div>
-                    <div className="row">
-                        <Product data={phone1}/>
-                        <Product data={phone1}/>
-                        <Product data={phone1}/>
-                        <Product data={phone1}/>
+                    <div className='row'>
+                    { items_list.length===0 ?   (  <p>Nol Products.</p>                                     
+                       )    
+                                                :                                
+                    (items_list.map((item) => (  <Product data={item}/>                    
+                     )))   
+                  } 
+                  </div> 
 
-                    </div>
-                    <div className="row">
-                        <Product data={phone1}/>
-                        <Product data={phone1}/>
-                        <Product data={phone1}/>
-                        <Product data={phone1}/>
 
-                    </div>
+
+
+
+
 
                 </div>
 
