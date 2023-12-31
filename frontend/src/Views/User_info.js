9e622c9395754fdd3af9b1de3e4ea408d5ccd2f8
rafikghaly <40712708+rafikghaly@users.noreply.github.com>
2023-12-31 03:59:34 +0200

    UserInfo Text boxes

--- frontend/src/Views/User_info.js
+++ frontend/src/Views/User_info.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react'
 import NavBar from "../Components/NavBar/Menu";
 
 import email_icon from "../Components/Assets/email.png";
@@ -6,38 +6,47 @@ import './page.css'
 import user_icon from "../Components/Assets/user.png";
 import location from "../Components/Assets/location.png";
 const User_info =() =>{
+
+    const [email,setEmail] = useState("");
+    const [name,setName] = useState("");
+
+
+
+
     return (
 
         <div>
             <NavBar/>
+            <form>
+                <div className='userinfo'>
+                    <h1 className="he">User Info</h1>
 
-            <div className='userinfo'>
-                <h1 className="he">User Info</h1>
+                    <div className="inputs">
 
-                <div className="inputs">
+                        <div className="info">
+                            <img src={user_icon} alt=""/>
+                            <input type="text" placeholder=''
+                            onChange = {e=>setName(e.target.value)}/>
+                            <button className="edit">Edit</button>
+                        </div>
 
-                    <div className="info">
-                        <img src={user_icon} alt=""/>
-                        <input type="text" readOnly={true}/>
-                        <button className="edit">Edit</button>
+                        <div className="info">
+                            <img src={email_icon} alt=""/>
+                            <input type="email"
+                            onChange = {e=>setEmail(e.target.value)}/>
+                            <button className="edit">Edit</button>
+                        </div>
+
+                        <div className="info">
+                            <img src={location} alt=""/>
+                            <input type="text" readOnly={true}/>
+                        </div>
+
+
                     </div>
 
-                    <div className="info">
-                        <img src={email_icon} alt=""/>
-                        <input type="email" readOnly={true}/>
-                        <button className="edit">Edit</button>
-                    </div>
-
-                    <div className="info">
-                        <img src={location} alt=""/>
-                        <input type="text" readOnly={true}/>
-                    </div>
-
-
-                </div>
-
-
-            </div>
+                 </div>
+            </form>
 
         </div>
     );
